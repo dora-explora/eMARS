ac8c7ffe433bd1783820656772963dc852dfe453
dora-explora <sarrafdara@gmail.com>
2025-06-22 00:58:10 -0700

    added a window with macroquad and egui

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@
 
 use std::env::args;
 use corewars_core::load_file::{Instruction};
+use macroquad::prelude::*;
+use egui::*;
+use egui_macroquad::*;
 
 mod sim;
 
@@ -11,7 +14,8 @@ fn print_core(core: &Vec<Instruction>) {
     }
 }
 
-fn main() {
+#[macroquad::main("eMARS")]
+async fn main() {
     let args: Vec<String> = args().collect();
     let mut core: Vec<Instruction>;
     match args.len() {
@@ -21,5 +25,18 @@ fn main() {
         _ => panic!("Too many arguments")
     }
 
-    print_core(&core);
+    loop {
+        clear_background(WHITE);
+
+        egui_macroquad::ui(|egui_ctx| {
+
+            egui::Window::new("Hello World!").show(egui_ctx, |ui|{
+                ui.label("and hello down here too");
+            });
+
+        });
+    
+        egui_macroquad::draw();
+        next_frame().await;
+    }
 }
