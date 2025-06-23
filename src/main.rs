ccf770275903ec7f61ab087701fe0afb73595a24
dora-explora <sarrafdara@gmail.com>
2025-06-23 00:54:05 -0700

    fixed the sigsegv & completed the core view window frick you macroquad

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 #![allow(unused_mut, unused_imports)]
 
 use std::env::args;
+use std::cmp::max;
 use corewars_core::load_file::{Instruction};
-use macroquad::{prelude::*, ui::root_ui, ui::hash, ui::widgets::Window};
-use egui::{*, vec2, Rect, Color32};
-use egui_macroquad::*;
+use eframe::egui;
+use egui::*;
 
 mod sim;
 
@@ -14,10 +14,7 @@ fn print_core(core: &Vec<Instruction>) {
     }
 }
 
-#[macroquad::main("eMARS")]
-async fn main() {
-    let args: Vec<String> = args().collect();
-    let mut core: Vec<Instruction>;
+fn load_core(args: Vec<String>) -> (isize, Vec<Instruction>) {
     let coresize: isize;
     match args.len() {
         ..=2 => panic!("Not enough arguments"),
@@ -25,47 +22,67 @@ async fn main() {
         4 => coresize = match args[3].parse::<isize>() { Ok(n) => n, Err(e) => panic!("Could not parse coresize argument: {e}")},
         _ => panic!("Too many arguments")
     }
-    core = sim::init(args[1].clone(), args[2].clone(), coresize);
+    return (coresize, sim::init(args[1].clone(), args[2].clone(), coresize));
+}
 
-    loop {
-        clear_background(WHITE);
+struct EmarsApp {
+    core: Vec<Instruction>,
+    coresize: isize
+}
 
-        let mut window_height = screen_height()-50.0;
-        let mut window_width = screen_height()*0.8;
-        
-        egui_macroquad::ui(|egui_ctx| {
-
-            egui::Window::new("Core View")
-            .movable(true)
-            .resizable(true)
-            .show(egui_ctx, |ui|{
-                let (response, painter) = ui.allocate_painter(vec2(window_width, window_height), Sense::hover());
-                
-                let top_left = response.rect.min;
-                let mut x = 2.0 + top_left.x;
-                let mut y = 2.0 + top_left.y;
-                for i in 0..coresize {
-                    // draws the rectangle at pos (x, y) and size (4, 4) in red
-                    painter.rect_filled(
-                        Rect::from_min_size(pos2(x, y), vec2(4., 4.)), 
-                        Rounding::none(), 
-                        Color32::RED
-                    );
-                    // moves next square's x 5 pixels to the left
-                    x += 5.0;
-                    if x > window_width - 5.0 + top_left.x { // if next square will overflow,
-                        x = 2.0 + top_left.x; // set it's x back to the beginning
-                        y += 5.0; // and move it down a row.
-                        if y > window_height + 5.0 { // if next row will overflow,
-                            break; // stop rendering the squares.
-                        }
+impl eframe::App for EmarsApp {
+    fn update(&mut self, context: &egui::Context, _: &mut eframe::Frame) {
+        egui::Window::new("Core View")
+        // .movable(true)
+        // .resizable(true)
+        .default_size(vec2(400., 400.))
+        .title_bar(false)
+        .show(context, |ui|{
+            
+            let painter_width = ui.available_width();
+            let painter_height = max((((self.coresize * 5) as f32 / painter_width).ceil() + 5.) as usize, ui.available_height() as usize) as f32;
+            
+            let (response, painter) = ui.allocate_painter(vec2(painter_width, painter_height), Sense::hover());
+            
+            let top_left = response.rect.min;
+            let mut x = 2. + top_left.x;
+            let mut y = 2. + top_left.y;
+            
+            let window_width = response.rect.max.x;
+            let window_height = response.rect.max.y;
+            
+            for i in 0..self.coresize {
+                // draws the rectangle at pos (x, y) and size (4, 4) in red
+                painter.rect_filled(
+                    Rect::from_min_size(pos2(x, y), vec2(4., 4.)), 
+                    CornerRadius::same(0), 
+                    Color32::RED
+                );
+                // moves next square's x 5 pixels to the left
+                x += 5.;
+                if (x - top_left.x) > window_width - 5. { // if next square will overflow,
+                    x = 2. + top_left.x; // set it's x back to the beginning
+                    y += 5.; // and move it down a row.
+                    if y > window_height + 5. { // if next row will overflow,
+                        break; // stop rendering the squares.
                     }
                 }
-            });
+            }
 
         });
-
-        egui_macroquad::draw();
-        next_frame().await;
     }
+}
+
+fn main() {
+    let args: Vec<String> = args().collect();
+    let (mut coresize, mut core) = load_core(args);    
+    
+    match eframe::run_native(
+        "eMARS", 
+        eframe::NativeOptions::default(), 
+        Box::new(|_cc| Ok(Box::new(EmarsApp { core, coresize })))
+    ) {
+        Err(error) => panic!("Error while rendering UI: {error}"),
+        Ok(_) => assert!(true)
+    };
 }
